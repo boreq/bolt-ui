627c9ebead7cc1844b9b2113f14f7d1d3ba0a8c8
boreq <boreq@0x46.net>
2021-01-20 02:42:17 +0100

    Update frontend

--- _tools/build_frontend.sh
+++ _tools/build_frontend.sh
@@ -3,11 +3,11 @@ set -e
 
 # Build frontend
 echo "Running yarn build"
-cd ../eggplant-frontend
+cd ../velo-frontend
 rm -rf dist
 yarn build
 
 # Build backend
-cd ../eggplant/ports/http/frontend
-echo "Running https://github.com/rakyll/statik"
-statik -f -src=../../../../eggplant-frontend/dist
+cd ../velo/ports/http/frontend
+echo "Running statik"
+statik -f -src=../../../../velo-frontend/dist
