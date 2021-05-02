cf447e57e7789374b01aa96bc4385385451b0961
boreq <boreq@0x46.net>
2021-05-02 16:49:56 +0200

    Update tools

--- _tools/build_frontend.sh
+++ _tools/build_frontend.sh
@@ -1,13 +1,54 @@
 #!/bin/bash
 set -e
 
-# Build frontend
-echo "Running yarn build"
+versionFile="ports/http/frontend/version.go"
+previousCommit=$(cat ${versionFile} | grep FrontendCommit | awk ' {gsub(/"/, "", $4); print $4 }')
+
 cd ../velo-frontend
+
+if ! git diff-index --quiet HEAD --; then
+    echo "You have uncommited changes in the frontend repository!"
+    exit 1
+fi
+
+commit=$(git rev-parse HEAD)
+echo "Previous frontend commit: ${previousCommit}"
+echo "Current frontend commit: ${commit}"
+
+if [ "$commit" = "$previousCommit" ]; then
+    echo "Frontend is already up to date (${commit} == ${previousCommit})"
+    exit 1
+fi
+
+echo "Generating a commit message ${previousCommit} -> ${commit}"
+
+commitFile="/tmp/velo-frontend-commit.txt"
+touch ${commitFile}
+echo "Update frontend" > ${commitFile}
+echo "" >> ${commitFile}
+git log --pretty=medium ${previousCommit}..${commit} >> ${commitFile}
+
+echo "Building the frontend"
 rm -rf dist
 yarn build
 
-# Build backend
-cd ../velo/ports/http/frontend
-echo "Running statik"
+cd ../velo
+
+if ! git diff-index --quiet HEAD --; then
+    echo "You have uncommited changes in the backend repository!"
+    exit 1
+fi
+
+echo "Running https://github.com/rakyll/statik"
+cd ./ports/http/frontend
 statik -f -src=../../../../velo-frontend/dist
+cd ../../../
+
+echo "Persisting frontend version"
+
+echo "package frontend" > "${versionFile}"
+echo "" >> "${versionFile}"
+echo "const FrontendCommit = \"${commit}\"" >> "${versionFile}"
+
+git add -u
+git commit -F ${commitFile}
---
+++ ports/http/frontend/version.go
@@ -0,0 +1,3 @@
+package frontend
+
+const FrontendCommit = "100e041680c0179594e8431ad1d01455e79e3c08"
