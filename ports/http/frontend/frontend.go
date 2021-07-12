7f4e53f8b1ed0e7694de82a24da4211883bbaab0
boreq <boreq@0x46.net>
2021-07-12 20:49:51 +0200

    Do not log frontend files being served

--- ports/http/frontend/frontend.go
+++ ports/http/frontend/frontend.go
@@ -3,28 +3,22 @@ package frontend
 import (
 	"embed"
 	"net/http"
-
-	"github.com/boreq/velo/logging"
 )
 
 //go:embed css/* js/* index.html favicon.ico
 var content embed.FS
 
 type FrontendFileSystem struct {
-	fs  http.FileSystem
-	log logging.Logger
+	fs http.FileSystem
 }
 
 func NewFrontendFileSystem() (*FrontendFileSystem, error) {
 	return &FrontendFileSystem{
-		fs:  http.FS(content),
-		log: logging.New("frontend"),
+		fs: http.FS(content),
 	}, nil
 }
 
 func (f *FrontendFileSystem) Open(name string) (http.File, error) {
-	f.log.Debug("serving frontend file", "name", name)
-
 	file, err := f.fs.Open(name)
 	if err != nil {
 		file, err := f.fs.Open("/index.html")
