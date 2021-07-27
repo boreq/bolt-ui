af6616003162ec263cfcfcad5ce0888388860843
boreq <boreq@0x46.net>
2021-07-27 23:57:02 +0200

    Cleanup

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,12 +3,8 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import VTooltip from 'v-tooltip';
-import VueTimeago from 'vue-timeago';
-import { distanceFilter, durationFilter, dateFilter } from './filters';
 
 Vue.use(VTooltip);
-
-Vue.use(VueTimeago, {});
 
 Vue.config.productionTip = false;
 
@@ -16,9 +12,4 @@ new Vue({
     router,
     store,
     render: (h) => h(App),
-    filters: {
-        distanceFilter,
-        durationFilter,
-        dateFilter,
-    },
 }).$mount('#app');
