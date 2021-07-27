fee7e66095ca63d5bac4951f311d89aada2c45d3
boreq <boreq@0x46.net>
2021-07-27 22:48:18 +0200

    Tools

--- _tools/build_frontend.sh
+++ _tools/build_frontend.sh
@@ -6,19 +6,7 @@ if ! git diff-index --quiet HEAD --; then
     exit 1
 fi
 
-versionFile="ports/http/frontend/version.go"
-previousCommit=$(cat ${versionFile} | grep FrontendCommit | awk ' {gsub(/"/, "", $4); print $4 }')
-
 cd ./frontend
-
-commit=$(git rev-parse HEAD)
-echo "Previous frontend commit: ${previousCommit}"
-echo "Current frontend commit: ${commit}"
-
-if [ "$commit" = "$previousCommit" ]; then
-    echo "Frontend is already up to date (${commit} == ${previousCommit})"
-    exit 0
-fi
 
 echo "Building..."
 rm -rf dist
@@ -28,11 +16,6 @@ echo "Copying files..."
 cd ../ports/http/frontend
 cp -r ../../../frontend/dist/. ./
 cd ../../../
-
-echo "Persisting frontend version..."
-echo "package frontend" > "${versionFile}"
-echo "" >> "${versionFile}"
-echo "const FrontendCommit = \"${commit}\"" >> "${versionFile}"
 
 echo "Commiting..."
 commitFile="/tmp/velo-frontend-commit.txt"
