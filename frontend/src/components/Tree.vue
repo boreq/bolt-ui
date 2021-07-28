f8e04bce7e9dad3a9186e900296eba69b7df40a0
boreq <boreq@0x46.net>
2021-07-28 22:38:41 +0200

    Add spinners

--- frontend/src/components/Tree.vue
+++ frontend/src/components/Tree.vue
@@ -1,8 +1,11 @@
 <template>
     <div class="tree" ref="tree" @scroll="onScroll">
+        <spinner v-if="loadingPrevious" class="previous-spinner"></spinner>
         <entries :entries="tree.entries" :selected="selectedInTree" 
             @entry="onEntry($event)" v-if="tree">
         </entries>
+        <spinner v-if="loadingNext" class="next-spinner"></spinner>
+
         <spinner v-if="!tree" class="main-spinner"></spinner>
     </div>
 </template>
