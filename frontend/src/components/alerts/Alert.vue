66b8b0dce5b44b97bc9852415bff9aa6354d3620
boreq <boreq@0x46.net>
2021-08-08 18:58:38 +0200

    Remove alerts

--- frontend/src/components/alerts/Alert.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div class="alert" :class="kind">
-        <slot></slot>
-    </div>
-</template>
-<script lang="ts" src="./Alert.ts"></script>
-<style scoped lang="scss" src="./Alert.scss"></style>
--- frontend/src/components/alerts/WarningAlert.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <alert kind="warning">
-        <slot></slot>
-    </alert>
-</template>
-<script lang="ts" src="./WarningAlert.ts"></script>
